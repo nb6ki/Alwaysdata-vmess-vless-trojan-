9c131824f759e31c90d0614c5a7eb4ef8932980a
nb6ki <98608612+nb6ki@users.noreply.github.com>
2022-06-03 17:17:03 +0800

    Update vless.sh

--- vless.sh
+++ vless.sh
@@ -5,7 +5,7 @@ wget -N https://raw.githubusercontent.com/bhqz/Alwaysdata-vmess-vless-trojan-/ma
 chmod +x ./web
 
 if [[ -z $id ]]; then
-    id="4fd46ad4-3272-4f3c-af05-241a14428f69"
+    id="333c130d-e3b2-4341-a221-dccc36cfab72"
 fi
 
 cat <<EOF > ~/config.json
